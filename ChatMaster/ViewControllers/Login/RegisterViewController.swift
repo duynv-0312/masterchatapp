bb36499592882dad498dabcf2464e2f482822a4e
dev_duynv <duynv.paraline@gmail.com>
2023-06-22 11:23:48 +0700

    wb #4 [Set Up Firebase, Email/Pass Log In]

--- ChatMaster/ViewControllers/Login/RegisterViewController.swift
+++ ChatMaster/ViewControllers/Login/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -201,7 +202,21 @@ class RegisterViewController: UIViewController {
             alertUserLoginError()
             return
         }
+        
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult,
+                  error == nil else {
+                print("Error creating user")
+                return
+            }
+            let user = result.user
+            print("Created user\(user)")
+        })
     }
+    
+    //Firebase Log In
+    
+    
     
     private func alertUserLoginError() {
         let alert = UIAlertController(title: "Woops",
